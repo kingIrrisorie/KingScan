a3253ebdb61a1c1e74c613a74230c59c643f97d0
King Irrisorie <iluminadopelalua@gmail.com>
2024-08-15 16:03:27 -0300

    iniciei a criacao do model manga e chapter

---
+++ APIManga/Model/Chapter.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIManga.Model
+{
+	public class Chapter
+	{
+		[Key]
+		public int Id { get; private set; }
+
+		public int MangId { get; set; }
+
+		public int Number {  get; set; }
+
+		public Manga manga { get; set; }
+	}
+}
---
+++ APIManga/Model/Manga.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace APIManga.Model
+{
+	public enum StatusManga
+	{
+		EmProgresso,
+		Finalizado,
+		Abandonado
+	}
+	public class Manga
+	{
+		[Key]
+		public int Id { get; private set; }
+
+		[Required]
+		public string Title { get; set; }
+
+		[Column(TypeName = "nvarchar(20)")]
+		public StatusManga Status { get; set; }
+
+		public string? Description { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime? Released {  get; set; }
+
+		public ICollection<Chapter> Chapters { get; set; }
+	}
+}
