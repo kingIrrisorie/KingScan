e363480a2322dfd3cf944137ddf0920a4c3baa22
King Irrisorie <iluminadopelalua@gmail.com>
2025-04-30 16:04:05 -0300

    adicionei um dockerfile

---
+++ APIManga/dockerfile.cs
@@ -0,0 +1,9 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:8.0
+COPY . /app
+WORKDIR /app
+EXPOSE 80
+ENTRYPOINT ["dotnet", "SuaApi.dll"]FROM mcr.microsoft.com/dotnet/aspnet:8.0
+COPY . /app
+WORKDIR /app
+EXPOSE 80
+ENTRYPOINT ["dotnet", "APIManga.dll"]
